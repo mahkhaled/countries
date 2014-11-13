f645d42064c33c16f9b30bb0180969f1c8dc6bbb
Mahmoud Khaled <mahmoud.khaled@badrit.com>
2014-11-13 16:59:50 +0200

    Update countries.rb

--- lib/countries.rb
+++ lib/countries.rb
@@ -3,7 +3,7 @@ require "countries/version"
 require 'iso3166'
 require 'countries/mongoid' if defined?(Mongoid)
 
-class Country < ISO3166::Country
+class ISOCountry < ISO3166::Country
   def to_s
     self.name
   end
